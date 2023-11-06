47eb0d2bae6121a957393d74c50db6df06d7eece
kritik123 <kritikpal123@gmail.com>
2023-11-06 12:15:36 +0530

    06/11/2023 kritik

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,19 +3,69 @@ import AboutTitle from "./AboutTitle";
 import CircularImageView from "./CircularImageView";
 
 function About(props) {
-    return <div>
-        <div className="d-flex justify-content-around m-5">
-            <AboutTitle />
-            <CircularImageView
-                imageUrl="https://assets-global.website-files.com/622fbc5a718ac3417fcd3d75/62327a2fb00126408cd587f2_john-carter-avatar-big-developer-webflow-ecommerce-template.jpg"
-                altText="User Profile"
-                borderRadius="50%"
-                radius="500px"
-            />
-        </div>
-        <Divider />
+  return (
+    <div>
+      <div className="row m-5 p-5">
+        <div className="col-lg-6 col-md-12">
+          <AboutTitle />
+          <div className="d-flex text-light me-5">
+            <div
+              className="d-flex align-items-center me-5"
+              style={{
+                maxWidth: "150px",
+              }}
+            >
+              <div style={expHeadText()}>2</div>
+              <div style={expHeadText()} className="text-primary">
+                +
+              </div>
+              <div style={expSubText()} className="mx-2">
+                Years Of Experiences
+              </div>
+            </div>
 
-    </div>;
+            <div
+              className="d-flex align-items-center mx-5"
+              style={{
+                maxWidth: "150px",
+              }}
+            >
+              <div style={expHeadText()} className="display-2">
+                50
+              </div>
+              <div style={expHeadText()} className="h1 text-primary">
+                +
+              </div>
+              <div style={expSubText()} className="mx-2">
+                Successful Projects
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-12 d-flex justify-content-between">
+          <CircularImageView
+            imageUrl="https://assets-global.website-files.com/622fbc5a718ac3417fcd3d75/62327a2fb00126408cd587f2_john-carter-avatar-big-developer-webflow-ecommerce-template.jpg"
+            altText="User Profile"
+            borderRadius="50%"
+            radius="500px"
+          />
+        </div>
+      </div>
+    </div>
+  );
+
+  function expHeadText() {
+    return {
+      fontSize: "60px",
+      fontWeight: "bolder",
+    };
+  }
+  function expSubText() {
+    return {
+      fontSize: "20px",
+      fontWeight: "bold",
+    };
+  }
 }
 
 export default About;
